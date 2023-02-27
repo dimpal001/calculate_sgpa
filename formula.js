8a9cbf01a24a35832d994280c3c51da500ea6f39
dimpal001 <dimpaldas518@gmail.com>
2023-02-28 01:36:01 +0530

    bigger inputs

--- formula.js
+++ formula.js
@@ -7,19 +7,21 @@ $(function(){
 
         }
         else{
+            $("#submit-btn").addClass("d-none");
+            $("#reset").removeClass("d-none");
+            $("#result-section").removeClass("d-none");
             $("#subjects").removeClass("d-none");
             $("#input-sub").css("pointer-events","none");
             for(let i = 1; i < input; i++){
                 $("#subjects").clone().prependTo("#incoming-row");
-                let a = "m"+i;
-                $(".marks").removeClass("m0");
-                $(".marks").addClass(a);
             }
-            $("#submit-btn").addClass("d-none");
-            $("#reset").removeClass("d-none");
         }
     });
+
     $("#reset").click(function(){
+        $(".marks").val("");
+        $(".credit").val("");
+        $("#result-section").addClass("d-none");
         $("#input-sub").css("pointer-events","auto");
         let input = $("#input-sub").val();
         for(let i = 1; i < input; i++){
@@ -30,4 +32,12 @@ $(function(){
         $("#submit-btn").removeClass("d-none");
         $("#reset").addClass("d-none");
     });
+
+    $("#calculate").click(function(){
+        let input = $("#input-sub").val();
+        let sum;
+        $(".marks input[type=number]").each(function(){
+            alert("this.value");
+        });
+    })
 });
