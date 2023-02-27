e961daa46a0c72b7be3f916752abbc87d0242df3
dimpal001 <dimpaldas518@gmail.com>
2023-02-28 00:11:39 +0530

    minor update

--- formula.js
+++ formula.js
@@ -3,9 +3,34 @@ $(function(){
         let input = $("#input-sub").val();
         if(input == ""){
         }
-        else{
-            alert("hi");
-            $('#subjects').prepend("<div class='col-6'></div>");
+        else if(input > 7){
+
         }
+        else{
+            $("#subjects").removeClass("d-none");
+            $("#input-sub").css("pointer-events","none");
+            for(let i = 1; i < input; i++){
+                $("#subjects").clone().prependTo("#incoming-row");
+                
+                $(".mark").addClass()
+            }
+            $("#submit-btn").addClass("d-none");
+            $("#reset").removeClass("d-none");
+
+            for(let i = 0; i < input; i++){
+                
+            }
+        }
+    });
+    $("#reset").click(function(){
+        $("#input-sub").css("pointer-events","auto");
+        let input = $("#input-sub").val();
+        for(let i = 1; i < input; i++){
+            $("#subjects").remove();
+        }
+        $("#subjects").addClass("d-none");
+        $("#input-sub").val("");
+        $("#submit-btn").removeClass("d-none");
+        $("#reset").addClass("d-none");
     });
 });
